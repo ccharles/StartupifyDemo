b453e706f78584ce0c0778e8195b9c677d2513a4
Chris Charles <chris@ccharles.ca>
2011-10-22 17:58:41 -0400

    Added the 'beer' app to my project.

--- beer/models.py
+++ beer/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Brewery(models.Model):
+
+    """
+    Brewery model.
+    """
+
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    pic = models.ImageField(upload_to='brewery/')
+    website = models.CharField(max_length=200) # Apparently the URLField type
+                                               # has been deprecated as of
+                                               # Django 1.3.1, but it is
+                                               # unclear what should be used
+                                               # instead. CharField makes as
+                                               # much sense as anything.
+
+
+class Beer(models.Model):
+
+    """
+    Beer model.
+    """
+
+    name = models.CharField(max_length=100)
+    brewery = models.ForeignKey(Brewery)
+    style = models.CharField(max_length=25)
+    pic = models.ImageField(upload_to='beer/')
+    abv = models.FloatField()
+
+
+class Review(models.Model):
+
+    """
+    Beer review model.
+    """
+
+    CONTAINER_CHOICES = (
+        ('CAN', 'Can'),
+        ('BTL', 'Bottle'),
+        ('TAP', 'Draught'),
+        ('CSK', 'Cask'),
+        ('GLR', 'Growler'),
+        ('OTR', 'Other'),
+    )
+
+    beer = models.ForeignKey(Beer)
+    date = models.DateField()
+    content = models.TextField()
+    rating = models.PositiveIntegerField()
+    pic = models.ImageField(upload_to='review/')
+    container = models.CharField(max_length=3, choices=CONTAINER_CHOICES)
+    volume = models.CharField(max_length=15)
