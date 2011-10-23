69952211babe94641ab1c7dbfc4836a6d6ebe569
Chris Charles <chris@ccharles.ca>
2011-10-22 22:05:20 -0400

    Added Brewery, ABV and Style columns to Beer admin interface.

--- beer/admin.py
+++ beer/admin.py
@@ -1,6 +1,11 @@
 from beer.models import Brewery, Beer, Review
 from django.contrib import admin
 
+
+class BeerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brewery', 'abv', 'style')
+
+
 admin.site.register(Brewery)
-admin.site.register(Beer)
+admin.site.register(Beer, BeerAdmin)
 admin.site.register(Review)
