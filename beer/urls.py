ccharles/StartupifyDemo

commit 5b9935ed0deb6faa6bc9791520571d69d7d0d237

Made Brewery, Beer and Index views generic.

beer/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls.defaults import *
+from django.views.generic import DetailView, ListView
+from beer.models import Brewery, Beer, Review, ReviewComment
 
-urlpatterns = patterns('beer.views',
-    # Examples:
-    # url(r'^$', 'startupifydemo.views.home', name='home'),
-    # url(r'^startupifydemo/', include('startupifydemo.foo.urls')),
-    url(r'^$', 'index'),
-    url(r'^brewery/(?P<brewery_id>\d+)/$', 'brewery'),
-    url(r'^beer/(?P<beer_id>\d+)/$', 'beer'),
+urlpatterns = patterns('',
+    url(r'^$',
+        ListView.as_view(
+            queryset=Review.objects.order_by('-date')[:5],
+            context_object_name='latest_reviews',
+            template_name='beer/index.html')),
+    url(r'^brewery/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Brewery,
+            template_name='beer/brewery.html')),
+    url(r'^beer/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Beer,
+            template_name='beer/beer.html')),
     url(r'^review/(?P<review_id>\d+)/$', 'review'),
     url(r'^review/(?P<review_id>\d+)/comment/$', 'review_comment'),
 )

beer/views.py
@@ -1,22 +1,9 @@
-from beer.models import Brewery, Beer, Review, ReviewComment
+from beer.models import Review, ReviewComment
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-
-def index(request):
-    latest_reviews = Review.objects.order_by('-date')[:5]
-    return render_to_response('beer/index.html',
-                              {'latest_reviews': latest_reviews})
-
-def brewery(request, brewery_id):
-    b = get_object_or_404(Brewery, pk=brewery_id)
-    return render_to_response('beer/brewery.html', {'brewery': b})
-
-def beer(request, beer_id):
-    b = get_object_or_404(Beer, pk=beer_id)
-    return render_to_response('beer/beer.html', {'beer': b})
 
 def review(request, review_id):
     r = get_object_or_404(Review, pk=review_id)

commit bbe88e86b59f220f57361d03abda833cd1437a41

Fixed issue with review, review_comment view processing introduced in 5b9935e.

beer/urls.py
@@ -16,6 +16,9 @@ urlpatterns = patterns('',
         DetailView.as_view(
             model=Beer,
             template_name='beer/beer.html')),
+)
+
+urlpatterns += patterns('beer.views',
     url(r'^review/(?P<review_id>\d+)/$', 'review'),
     url(r'^review/(?P<review_id>\d+)/comment/$', 'review_comment'),
 )