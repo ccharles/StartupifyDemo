f000ee64dfb46e35784f47401b11b268979fb28a
Chris Charles <chris@ccharles.ca>
2011-10-25 12:24:39 -0400

    Added ReviewComment form and a view to handle submisson.

--- beer/urls.py
+++ beer/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('beer.views',
     url(r'^brewery/(?P<brewery_id>\d+)/$', 'brewery'),
     url(r'^beer/(?P<beer_id>\d+)/$', 'beer'),
     url(r'^review/(?P<review_id>\d+)/$', 'review'),
+    url(r'^review/(?P<review_id>\d+)/comment/$', 'review_comment'),
 )
--- beer/views.py
+++ beer/views.py
@@ -1,6 +1,8 @@
 from beer.models import Brewery, Beer, Review, ReviewComment
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
 
 
 def brewery(request, brewery_id):
@@ -14,5 +16,29 @@ def beer(request, beer_id):
 def review(request, review_id):
     r = get_object_or_404(Review, pk=review_id)
     comments = ReviewComment.objects.filter(review=r)
-    return render_to_response('beer/review.html', {'review': r,
-                                                   'comments': comments})
+
+    return render_to_response('beer/review.html',
+                              {'review': r, 'comments': comments},
+                              context_instance=RequestContext(request))
+
+def review_comment(request, review_id):
+    r = get_object_or_404(Review, pk=review_id)
+
+    try:
+        comment_author = request.POST['author']
+        comment_text = request.POST['content']
+    except (KeyError, e):
+        return render_to_response('beer/review.html',
+                                  {'review': r,
+                                   'comments': comments,
+                                   'error_message': 'Could not save comment'},
+                                  context_instance=RequestContext(request))
+
+    else:
+        new_comment = ReviewComment()
+        new_comment.review = r
+        new_comment.author = comment_author
+        new_comment.content = comment_text
+        new_comment.save()
+
+        return HttpResponseRedirect(reverse('beer.views.review', args=(r.id,)))
